60191ee892ca89b9b6fcd69186eb0b6916c7c02f
Lucas Braun <lucasalan@gmail.com>
2018-02-04 15:03:53 +0100

    Develop methods for four-way plot

---
+++ Homeworks/homework3.R
@@ -0,0 +1,66 @@
+# SIW004: Homework 3
+# -------------------------------
+# H3: Final Homework (individual)
+# -------------------------------
+
+# 0. Load data
+sids <- read.table("/Users/lucasbraun/Google\ Drive/Grad\ School/Applied\ Mathematics/RSPCMA/Data/chap7sids.csv", header = TRUE, sep = ",")
+
+# 1. Extract as much information as possible from the data
+# set using graphical tools as described in Chapter 2 of Everitt.
+
+# One of the questions about these data might be “How is birthweight related to heart rate?”
+# A first step in answering the question would be to examine a scatterplot of the two variables.
+# Here, in fact, we will produce four versions of the basic scatterplot:
+
+PlotFourWays <- function(data, x.label, y.label) {
+  x = do.call("$", list(data, x.label))
+  y = do.call("$", list(data, y.label))
+
+  # Set up plotting area to take four graphs
+  par(mfrow=c(2,2))
+  par(pty="s")
+  SimplePlot("(a)", x, y, x.label, y.label)
+
+  SimplePlot("(b)", x, y, x.label, y.label)
+  # Add regression line
+  abline(lm(y~x),lwd=2)
+
+  # Jitter data
+  data.jitter<-jitter(cbind(x,y))
+  SimplePlot("(c)", data.jitter[,1], data.jitter[,2], x.label, y.label)
+
+  SimplePlot("(d)", x, y, x.label, y.label)
+  # Add rug plots
+  rug(jitter(x), side=1)
+  rug(jitter(y), side=2)
+
+  file.name <- paste(x.label, '_vs_', y.label, '.png', sep="")
+  dev.copy(png, file.name)
+  dev.off()
+}
+
+SimplePlot <- function(title, x, y, x.label, y.label) {
+  plot(x, y, xlab=x.label, ylab=y.label, pch=1, lwd=2)
+  title(title, lwd=2)
+}
+
+names <- names(sids)
+
+for (x.variable in names) {
+  for (y.variable in names) {
+    if (x.variable != y.variable) {
+      PlotFourWays(sids, x.variable, y.variable)
+    }
+  }
+}
+
+
+# 2. Perform a PCA and interpret the results.
+
+# 3. Find homogeneous clusters amongst the individuals without
+# considering the variable Group (use hierarchical and k-means methods
+# under two distinct choices of distance methods). Compare the results
+# with the existing groups given by variable "group". Then perform LDA
+# and classify into these groups the following two new observations:
+# Obs1: (110,3320,0.240,39); Obs2: (120,3310,0.298,37).
